90ebe9e7864d40d6802d7b03c210627461dd674a
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-04-26 15:07:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
 
+  get "/newteam" do
+    @name = params[:name]
+    erb :newteam
+  end
+  
+  post "/team" do 
+
+  end
+  
 end
