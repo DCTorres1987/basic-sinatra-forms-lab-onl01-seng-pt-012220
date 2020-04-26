0c4d3ed8d1f8ada1079fa8d6f4fba9bf6db336e1
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-04-26 15:26:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,9 +8,13 @@ class App < Sinatra::Base
   end
   
   post "/team" do 
-    @name = params["name"]
+    @name =  params["name"]
     @coach = params["coach"]
-    @pg = params["pg"]
+    @pg =    params["pg"]
+    @sg =    params["sg"]
+    @pf =    params["pf"]
+    @sf =    params["sf"]
+    @c =     params["c"]
     erb :team
   end
   
